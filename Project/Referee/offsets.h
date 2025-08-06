74b58479278c8f397451abfe53d93f827d52d94a
Ryan <ryanhennessy282@gmail.com>
2025-08-06 23:59:49 +0100

    Update offsets.h
    updated offsets for referee checks and SSL Pinning

--- Project/Referee/offsets.h
+++ Project/Referee/offsets.h
@@ -4,10 +4,10 @@
 namespace offsets
 {
 	//you can get these offsets referring to ryans tutorial!
-	static constexpr inline uint64_t refOne = 0x771CB0;
-	static constexpr inline uint64_t refTwo = 0x760D10;
-	static constexpr inline uint64_t refThree = 0x76F440;
+	static constexpr inline uint64_t refOne = 0x76E130;
+	static constexpr inline uint64_t refTwo = 0x75D160;
+	static constexpr inline uint64_t refThree = 0x76B870;
 
 	//you can get this inside dump, this is for ssl.
-	static constexpr inline uint64_t notifyServerCertificate = 0x7982540; //private class LegacyTlsAuthentication : Il2CppObject
+	static constexpr inline uint64_t notifyServerCertificate = 0x791BA50; //private class LegacyTlsAuthentication : Il2CppObject
 }
